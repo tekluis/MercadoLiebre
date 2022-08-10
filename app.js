4c7f5ef38f0ab3e7535339ef8e88ea0e0882ddb0
tekluis <tekluisabraham@gmail.com>
2022-08-10 19:34:53 -0300

    gitignore

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando en puerto 3000');
+app.listen(3030, ()=>{
+    console.log('Servidor funcionando en puerto 3030');
 });
 
 app.get('/', (req,res)=>{
