56f8f688b9dbb91c5af10b362541fd6f2d986279
tekluis <tekluisabraham@gmail.com>
2022-08-10 19:30:09 -0300

    agregado puerto en consola

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use('/static', express.static(__dirname + '/public'));
 
 app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+    console.log('Servidor funcionando en puerto 3000');
 });
 
 app.get('/', (req,res)=>{
