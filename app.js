d05e9af321a756f2d00da7b08290dfd4938419f7
tekluis <tekluisabraham@gmail.com>
2022-08-11 20:30:55 -0300

    formato css primera version

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.listen(3030, ()=>{
-    console.log('Servidor funcionando en puerto 3030');
+app.listen(3050, ()=>{
+    console.log('Servidor funcionando en puerto 3050');
 });
 
 app.get('/', (req,res)=>{
