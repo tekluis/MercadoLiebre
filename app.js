0de72249da1392cade7113e8e8d6c315a6f4860c
tekluis <tekluisabraham@gmail.com>
2022-08-25 14:39:08 -0300

    agregado login y register

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ app.listen(3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
+
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
