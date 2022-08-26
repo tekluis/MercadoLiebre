82c8efd100c3681e6d584fa6040478725b2434a9
tekluis <tekluisabraham@gmail.com>
2022-08-26 18:09:59 -0300

    arreglo items registro

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+var puerto=3000;
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(puerto, ()=>{
+    console.log('Servidor funcionando en puerto '+puerto);
 });
 
 app.get('/', (req,res)=>{
